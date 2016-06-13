30d40d42c6ee00e59eb2bfbc133b33bc07aff23d
Hugh Perkins <hughperkins@gmail.com>
2016-06-13 10:31:21 +0800

    try to fix neonbase.py proc

--- backends/neonbase.py
+++ backends/neonbase.py
@@ -8,13 +8,16 @@ from neon.layers import Convolution
 from neon.initializers import Gaussian
 from neon.backends import gen_backend
 import pycuda.driver as cuda
-import pycuda.autoinit
+#import pycuda.autoinit
 import pycuda.gpuarray as gpuarray
 
 init = Gaussian()
 
 def test(batch_size, its, layer):
     np.random.seed(123)
+
+    gen_backend(backend='gpu', batch_size=batch_size,
+            datatype=np.float32, device_id=0)
 
     input_filters = layer['Ci']
     output_filters = layer['Co']
@@ -29,9 +32,6 @@ def test(batch_size, its, layer):
     gradOutputs = np.zeros((image_size * image_size * output_filters, batch_size), dtype=np.float32)
     gradOutputs[:] = np.random.randn(*gradOutputs.shape)
     gradOutputs_cuda = gpuarray.to_gpu(gradOutputs)
-
-    gen_backend(backend='gpu', batch_size=batch_size,
-            datatype=np.float32, device_id=0)
 
     conv = Convolution((3, 3, output_filters), strides=1, padding=1, init=init)
     conv.configure((input_filters, image_size, image_size))
