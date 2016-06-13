18af029397a798390b211eeda5cce65ab34e1a9c
Hugh Perkins <hughperkins@gmail.com>
2016-06-13 10:17:34 +0800

    first created

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # neon-benchmarks
 benchmarks for neon, both cuda and OpenCL version
+
+This is a separate repo, so it can be used against both repos
+
+Pluggable test backend lets tests run against both
+
+## How to use: winogradcl
+
+* first, install winogradcl https://github.com/hughperkins/winogradCl-underconstruction
+* now, using the same virtualenv you just set up, simply run, from this `neon-benchmarks` repo:
+```
+cd neon-benchmarks
+python run_benchmarks.py
+```
+That's it! :-)
+
+## How to use: neon base
+
+* Install neon base from https://github.com/nervanasystems/neon
+* Using the same venv you just created, ie do `source .venv/bin/activate` from the `neon` directory, do:
+```
+cd neon-benchmarks
+python run_benchmarks.py --backend neonbase
+```
+
---
+++ backends/neonbase.py
@@ -0,0 +1,42 @@
+"""
+Tests using neonbase, ie assumes we are on nervanasystems neon master branch
+"""
+import sys
+import time
+import numpy as np
+from neon.layers import Convolution
+from neon.initializers import Gaussian
+from neon.backends import gen_backend
+import pycuda.driver as cuda
+import pycuda.autoinit
+import pycuda.gpuarray as gpuarray
+
+init = Gaussian()
+
+def test(batch_size, its, layer):
+    np.random.seed(123)
+
+    input_filters = layer['Ci']
+    output_filters = layer['Co']
+    image_size = layer['iW']
+    assert layer['iH'] == image_size
+    assert layer['kH'] == layer['kW'] == 3
+
+    inputs = np.zeros((input_filters,image_size, image_size,batch_size), dtype=np.float32)
+    inputs[:] = np.random.randn(*inputs.shape)
+    inputs_cuda = gpuarray.to_gpu(inputs)
+
+    gradOutputs = np.zeros((image_size * image_size * output_filters, batch_size), dtype=np.float32)
+    gradOutputs[:] = np.random.randn(*gradOutputs.shape)
+    gradOutputs_cuda = gpuarray.to_gpu(gradOutputs)
+
+    gen_backend(backend='gpu', batch_size=batch_size,
+            datatype=np.float32, device_id=0)
+
+    conv = Convolution((3, 3, output_filters), strides=1, padding=1, init=init)
+    conv.configure((input_filters, image_size, image_size))
+    conv.allocate()
+    
+    conv.fprop(inputs_cuda)
+    conv.bprop(gradOutputs_cuda)
+
