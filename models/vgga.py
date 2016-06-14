6a956f63b666d66bf53fc7885b8e78e9dbc13068
Hugh Perkins <hughperkins@gmail.com>
2016-06-14 20:42:34 +0800

    layer 2 gradW eps

--- models/vgga.py
+++ models/vgga.py
@@ -16,6 +16,8 @@ def get_net():
             layer['epsGradI'] = 1e-4
             if conv_index == 1:
                 layer['epsGradW'] = 1e-2
+            elif conv_index == 2:
+                layer['epsGradW'] = 1e-3
             net.append(layer)
             channels = op
             conv_index += 1
