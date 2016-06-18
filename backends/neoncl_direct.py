6d05b1bca1b8c4dd5089f3aebc19fa2ab2a2aabc
Hugh Perkins <hughperkins@gmail.com>
2016-06-19 04:05:04 +0800

    bunch of cleaning; and remove ctx from calls to fprop/bprop

--- backends/neoncl_direct.py
+++ backends/neoncl_direct.py
@@ -36,41 +36,20 @@ class Test(object):
            i += len(gpu_devices)
 
         print('cl_context', ctx)
-        #ctx = cl.create_some_context()
         q = cl.CommandQueue(ctx)
 
         W_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=W)
         I_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=I)
-
-        # W_cl = MyClTensor.from_np(be, self.W)
-#        print('self.I', self.I)
-        # I_cl = MyClTensor.from_np(be, self.I)
-
-        # conv = Convolution((3, 3, output_filters), strides=1, padding=1, be=be) #, init=init)
-        # conv.configure((input_filters,image_size, image_size))
-        # conv.W = W_cl
+        gradO_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=gradO)
 
         O = np.zeros((image_size * image_size * output_filters, batch_size), dtype=np.float32)
         O_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=O)
-        # O_cl = MyClTensor.from_np(be, O)
-        # conv.outputs = O_cl
-
-        # conv.fprop(I_cl)
-        # be.q.finish()
-
-        # gradO_cl = MyClTensor.from_np(be, self.gradO)
-        gradO_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=gradO)
 
         gradI = np.zeros((input_filters,image_size, image_size,batch_size), dtype=np.float32)
         gradI_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=gradI)
-        # gradI_cl = MyClTensor.from_np(be, gradI)
 
         gradW = np.zeros((input_filters,3,3,output_filters), dtype=np.float32)
         gradW_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=gradW)
-        # gradW_cl = MyClTensor.from_np(be, gradW)
-        
-        # conv.deltas = gradI_cl
-        # conv.dW = gradW_cl
 
         self.convolver = api.Convolver(ctx, batch_size, input_filters, output_filters,
             layer_def['kH'], layer_def['kW'], layer_def['iH'], layer_def['iW'],
@@ -82,7 +61,6 @@ class Test(object):
         self.O = O
         self.q = q
         self.W_cl = W_cl
-        # self.convolver = convo
         self.gradW = gradW
         self.gradI = gradI
 
@@ -98,26 +76,21 @@ class Test(object):
         self.q.finish()
 
     def fprop(self):
-        self.convolver.fprop(self.ctx, self.q, self.I_cl, self.W_cl, self.O_cl)
+        self.convolver.fprop(self.q, self.I_cl, self.W_cl, self.O_cl)
 
     def bprop(self):
-        self.convolver.bprop_gradW(self.ctx, self.q, self.I_cl, self.gradO_cl, self.gradW_cl)
-        self.convolver.bprop_gradI(self.ctx, self.q, self.gradO_cl, self.W_cl, self.gradI_cl, self.scratch_cl)
-        pass
-        # self.conv.bprop(self.gradO_cl)
+        self.convolver.bprop_gradW(self.q, self.I_cl, self.gradO_cl, self.gradW_cl)
+        self.convolver.bprop_gradI(self.q, self.gradO_cl, self.W_cl, self.gradI_cl, self.scratch_cl)
 
     def getO(self):
-        # self.O_cl.to_host()
         cl.enqueue_copy(self.q, self.O, self.O_cl)
         return self.O
 
     def getGradW(self):
-        # self.gradW_cl.to_host()
         cl.enqueue_copy(self.q, self.gradW, self.gradW_cl)
         return self.gradW
 
     def getGradI(self):
-        # self.gradI_cl.to_host()
         cl.enqueue_copy(self.q, self.gradI, self.gradI_cl)
         return self.gradI
 
