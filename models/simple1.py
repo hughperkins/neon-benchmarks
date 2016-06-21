90c161c4a30b64e42d95d10ad2ef798c7d2ebab9
Hugh Perkins <hughperkins@gmail.com>
2016-06-21 09:46:06 +0800

    move all 'simple' models into one file

--- models/simple1.py
+++ models/simple1.py
@@ -5,8 +5,13 @@ def get_batchsize():
 
 def get_net():
     net = []
-    layer = {'Ci': 32, 'Co': 32, 'iH': 64, 'iW': 64, 'kH': 3, 'kW': 3, 'padH': 1, 'padW': 1,
-             'dH': 1, 'dW': 1}
-    net.append(layer)
+    net.append({'Ci': 32, 'Co': 32, 'iH': 64, 'iW': 64, 'kH': 3, 'kW': 3, 'padH': 1, 'padW': 1,
+             'dH': 1, 'dW': 1})
+    net.append({'Ci': 32, 'Co': 32, 'iH': 8, 'iW': 8, 'kH': 3, 'kW': 3, 'padH': 1, 'padW': 1,
+             'dH': 4, 'dW': 4})  # has stride > 1
+    net.append({'Ci': 4, 'Co': 4, 'iH': 4, 'iW': 4, 'kH': 3, 'kW': 3, 'padH': 1, 'padW': 1,
+             'dH': 4, 'dW': 4})  # smaller version of previous layer
+    net.append({'Ci': 32, 'Co': 32, 'iH': 64, 'iW': 64, 'kH': 3, 'kW': 3, 'padH': 0, 'padW': 0,
+             'dH': 1, 'dW': 1})  # zero padding
     return net
 
